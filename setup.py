b80e4177b672eca5ae2f3a22572110ebb0e4bf9a
Tomoya Kose <tomoya@mitsuse.jp>
2018-08-15 08:29:47 +0900

    1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='sphinx-paragraph-extractor',
-    version='0.1.0',
+    version='1.0.0',
     description='',
     url='https://github.com/mitsuse/sphinx-paragraph-extractor',
     author='Tomoya Kose',
